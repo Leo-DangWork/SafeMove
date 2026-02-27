1b3a4537e512badef583b9d0aedb73fc682f375f
Dzung03 <142816988+Dzung03@users.noreply.github.com>
2026-02-27 22:48:17 +0700

    Update Account.java

--- src/java/model/Account.java
+++ src/java/model/Account.java
@@ -9,5 +9,61 @@ package model;
  * @author Dzung
  */
 public class Account {
-    
+    private int account_id;
+    private String username;
+    private String password;
+    private String role;
+    private String status;
+
+    public Account() {
+    }
+
+    public Account(int account_id, String username, String password, String role, String status) {
+        this.account_id = account_id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.status = status;
+    }
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
+
